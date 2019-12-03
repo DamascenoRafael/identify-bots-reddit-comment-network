4d22659c01a45f0099539e01cf19fcc2cb60320d
DamascenoRafael <rdamasceno@poli.ufrj.br>
2019-12-03 17:31:42 -0300

    Add sample dataset and paths_constantes helper

---
+++ src/paths_constants.py
@@ -0,0 +1,30 @@
+from pathlib import Path
+
+data = Path('../data/')
+data_raw = data / 'raw'
+data_processed = data / 'processed'
+
+results = Path('../results/')
+
+bots_file = data_processed / 'all_bots'
+
+def data_processed_dataset(dataset):
+    return data_processed / dataset
+
+def dataset_edgelist(dataset):
+    return data_processed_dataset(dataset) / 'edgelist'
+
+def subgraph_edgelist(dataset, execution_type):
+    return data_processed_dataset(dataset) / ('subgraph_' + execution_type)
+
+def results_dataset(dataset):
+    return results / dataset
+
+def results_subgraph(dataset, execution_type):
+    return results_dataset(dataset) / ('subgraph_' + execution_type)
+
+def metrics_subgraph(dataset, execution_type):
+    return results_subgraph(dataset, execution_type) / 'metrics'
+
+def charts_subgraph(dataset, execution_type):
+    return results_subgraph(dataset, execution_type) / 'charts'
